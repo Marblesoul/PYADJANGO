8ac0d05e83a1a864057deb4a2b8e3fac9dde0deb
Dmitry <gworking7@gmail.com>
2025-03-07 00:19:06 +0300

    Work_with_database task done

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+    list_phones = Phone.objects.all()
+    if sort == 'name':
+        list_phones = list_phones.order_by('name')
+    elif sort == 'min_price':
+        list_phones = list_phones.order_by('price')
+    elif sort == 'max_price':
+        list_phones = list_phones.order_by('-price')
+    context = {
+        'phones': list_phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
