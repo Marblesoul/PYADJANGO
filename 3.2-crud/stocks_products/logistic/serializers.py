39889dbc66a144bd585c4ea8c2652c013aca3c8a
Dmitry <golyshevd@live.ru>
2025-05-11 11:03:17 +0300

    Logistic project done.

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,32 @@
+from itertools import product
+
 from rest_framework import serializers
+
+from .models import Product, StockProduct, Stock
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
-
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,6 +38,14 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price'],
+            )
 
         return stock
 
@@ -40,4 +60,21 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
+        current_positions = {pos.product_id: pos for pos in stock.positions.all()}
+
+        for position in positions:
+            product_id = position['product'].id
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product_id=product_id,
+                defaults={
+                    'quantity': position['quantity'],
+                    'price': position['price'],
+                },
+            )
+
+            current_positions.pop(product_id, None)
+
+        for position_to_delete in current_positions.values():
+            position_to_delete.delete()
         return stock
